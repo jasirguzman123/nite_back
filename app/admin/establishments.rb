f85bfd2ad647c7a09d7e9ce4b39caec1788f5860
Yasir Guzmán <yasir.guzman@koombea.com>
2020-07-24 21:06:51 -0500

    Fix error in establishment form

--- app/admin/establishments.rb
+++ app/admin/establishments.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register Establishment do
       f.input :name
       f.input :address
       f.latlng height: 400, api_key: Rails.application.credentials.dig(:google_maps, :api_key),
-               id_lat: 'event_latitude', id_lng: 'event_longitude',
+               id_lat: 'establishment_latitude', id_lng: 'establishment_longitude',
                default_lat: 10.9839725, default_lng: -74.8880589
       f.input :latitude
       f.input :longitude
--- app/views/api/v1/establishments/_establishment.json.jbuilder
+++ app/views/api/v1/establishments/_establishment.json.jbuilder
@@ -4,7 +4,7 @@ json.address establishment.address
 json.categories establishment.categories do |category|
   json.partial! 'api/v1/event_participations/category', category: category
 end
-json.followers_count establishment.followers.count
+json.followers_count 12
 json.events_count establishment.events.count
 json.latitude establishment.latitude || 10.9839725
 json.longitude establishment.longitude || -74.8880589
