3a55319445323ed774350031f0e84b6a45c9c2c7
Yasir Guzmán <yasir.guzman@koombea.com>
2020-08-02 17:35:29 -0500

    Add pagination to events controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::EventsController < Api::V1::ApiController
   skip_before_action :set_current_user
 
   def index
-    events
+    events_in_date
   end
 
   def show
@@ -12,7 +12,19 @@ class Api::V1::EventsController < Api::V1::ApiController
   private
 
   def events
-    @events ||= Event.includes(:localities, :categories, :owner).with_attached_images.all.ransack(params[:q]).result
+    @events ||= Event.includes(:localities, :categories, :owner)
+                     .with_attached_images.all.ransack(params[:q]).result
+                     .paginate(page: params[:page])
+  end
+
+  def events_in_date
+    return events unless date.present?
+
+    @events = events.where('date(starting_hour) = ?', date.to_date)
+  end
+
+  def date
+    params[:date]
   end
 
   def event
